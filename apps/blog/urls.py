4e42e8ac15b37f10148d457658b244a09e2ccff0
DotPack <lifirenko123@gmail.com>
2024-02-14 22:25:42 +0200

    add form

--- apps/blog/urls.py
+++ apps/blog/urls.py
@@ -7,4 +7,5 @@ app_name = 'blog'
 urlpatterns = [
     path('', views.index, name='index'),
     path('<int:post_id>/', views.post, name='post'),
+    path('create/', views.create, name='create'),
 ]
