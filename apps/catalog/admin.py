f218b624a105cb1cdf0de8e4a4ce52619073dd5a
Lifirenko Yegor <lifirenko123@gmail.com>
2024-03-16 12:46:18 +0200

    add catalog model and view

--- apps/catalog/admin.py
+++ apps/catalog/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from .models import Catalog
+
 # Register your models here.
+@admin.register(Catalog)
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)} # це поле автоматично заповнюється на основі іншого поля
+    # readonly_fields = ()
