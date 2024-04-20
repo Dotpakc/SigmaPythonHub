42e65ab0e66e15fbc253313a86bd5c461952d46d
DotPack <lifirenko123@gmail.com>
2024-04-20 13:30:41 +0300

    Update API catalog view and templates for carousel functionality

--- apps/api/catalog/views.py
+++ apps/api/catalog/views.py
@@ -11,6 +11,6 @@ from .serializers import CatalogSerializer, ProductSerializer
 
 #10 random products
 class ProductViewSet(viewsets.ModelViewSet):
-    queryset = Product.objects.all().order_by('?').prefetch_related('category').prefetch_related('images')[:10]
+    queryset = Product.objects.all().order_by('?').prefetch_related('category').prefetch_related('images')
     serializer_class = ProductSerializer
     permission_classes = []
