6bcfe057ec364bffec1ae52c920d0ab3088a7e25
Lifirenko Yegor <lifirenko123@gmail.com>
2024-04-28 16:54:26 +0300

    Update staticfiles destination path in vercel.json

--- core/settings.py
+++ core/settings.py
@@ -167,7 +167,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = BASE_DIR / 'staticfiles_build' / 'static'
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
