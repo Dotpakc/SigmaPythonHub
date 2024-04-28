689b12af1e0694c4c71423ce66c745a0da73adb0
Lifirenko Yegor <lifirenko123@gmail.com>
2024-04-28 15:03:13 +0300

    Update build_files.sh to include new commands for creating a superuser, collecting static files, and changing the admin password

--- build_files.sh
+++ build_files.sh
@@ -2,4 +2,6 @@ python3 -m pip install -r requirements.txt
 
 python3 manage.py makemigrations
 python3 manage.py migrate
-python3 manage.py createsuperuser --noinput --username admin --email lol@lol.com --password admin 
+python3 manage.py createsuperuser --noinput --username admin --email lol@lol.com
+python3 manage.py collectstatic --noinput
+python3 manage.py changepassword admin admin
