1faf35aa7e95f3da52471b306570e2126b2bc645
Lifirenko Yegor <lifirenko123@gmail.com>
2024-04-28 15:04:38 +0300

    Refactor build_files.sh to include new commands for creating a superuser and collecting static files

--- build_files.sh
+++ build_files.sh
@@ -4,4 +4,3 @@ python3 manage.py makemigrations
 python3 manage.py migrate
 python3 manage.py createsuperuser --noinput --username admin --email lol@lol.com
 python3 manage.py collectstatic --noinput
-python3 manage.py changepassword admin admin
