22c31c07818082b16f09e5f899ae9f667217537d
Lifirenko Yegor <lifirenko123@gmail.com>
2024-04-28 14:53:27 +0300

    Update API URL to use relative path in index.html

--- build_files.sh
+++ build_files.sh
@@ -2,3 +2,4 @@ python3 -m pip install -r requirements.txt
 
 python3 manage.py makemigrations
 python3 manage.py migrate
+python3 manage.py createsuperuser --noinput --username admin --email lol@lol.com --password admin 
